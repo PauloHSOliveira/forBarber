3b925de1fb3752dc2d821db53ccce755eebbf3e2
codephs <codephs@users.noreply.github.com>
2020-01-29 15:53:13 -0300

    ip file 192.168.1.106

--- backend/src/app/models/File.js
+++ backend/src/app/models/File.js
@@ -11,7 +11,7 @@ class File extends Model {
           get() {
             // eslint-disable-next-line spaced-comment
             //return `${process.env.APP_URL}/files/${this.path}`;
-            return `http://192.168.1.105:3333/files/${this.path}`;
+            return `http://192.168.1.106:3333/files/${this.path}`;
           },
         },
       },
